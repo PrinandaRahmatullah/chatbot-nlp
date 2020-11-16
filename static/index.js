0838c90a8ee58f5996d8a96ae70fb199fdd222dc
Prinanda Rahmatullah <prinanda50@gmail.com>
2020-11-17 02:34:00 +0700

    convert jquery to javascript and input validation

--- static/index.js
+++ static/index.js
@@ -1,58 +1,60 @@
-// showChat();
-
 // Tombol Chat
-$("#prime").click(function () {
-	toggleFab();
+document.querySelector("#prime").addEventListener("click", () => {
+	toggleFab()
 });
 
 // Ubah bentuk tombol chat
 function toggleFab() {
-	$(".prime").toggleClass("zmdi-comments");
-	$(".prime").toggleClass("zmdi-close");
-	$(".prime").toggleClass("is-active");
-	$(".prime").toggleClass("is-visible");
-	$("#prime").toggleClass("is-float");
-	$(".chat").toggleClass("is-visible");
-	$(".fab").toggleClass("is-visible");
+	document.querySelector(".prime").classList.toggle("zmdi-comments")
+	document.querySelector(".prime").classList.toggle("zmdi-close")
+	document.querySelector(".prime").classList.toggle("is-active")
+	document.querySelector(".prime").classList.toggle("is-visible")
+	document.querySelector("#prime").classList.toggle("is-float")
+	document.querySelector(".chat").classList.toggle("is-visible")
+	document.querySelector(".fab").classList.toggle("is-visible")
 }
 
 // Connext to socket
 var socket = io();
 socket.on("connect", function () {
-	var sendButton = $("#fab_send");
-	var chatbotInput = $("#chatSend");
+	var sendButton = document.querySelector("#fab_send")
+	var chatbotInput = document.querySelector("#chatSend")
 
-	// if ($.trim(chatbotInput.val()) != "") {}
-	chatbotInput.keypress(function (event) {
+	chatbotInput.addEventListener("keypress", (event) => {
 		var keycode = event.which;
 		if (keycode == 13) {
-			sendButton.click();
+			chatbotInput.value = chatbotInput.value.replace(/^\s+/g, "")
+			if (chatbotInput.value === "") {} else if (chatbotInput.value != null && chatbotInput.value != "") {
+				sendButton.click()
+			}
 		}
 	});
 
-	sendButton.click(function () {
+	sendButton.addEventListener("click", () => {
 		// message yg user input
-		socket.emit("send message", chatbotInput.val());
+		chatbotInput.value = chatbotInput.value.replace(/^\s+/g, "")
+		if (chatbotInput.value != null && chatbotInput.value != "") {
+			socket.emit("send message", chatbotInput.value);
 
-		$("div#chat_fullscreen").append(
-			'<span class="chat_msg_item chat_msg_item_user">' +
-			chatbotInput.val() +
-			"</span>"
-		);
-		chatbotInput.val("");
+			document.querySelector("div#chat_fullscreen").insertAdjacentHTML("beforeend",
+				'<span class="chat_msg_item chat_msg_item_user">' +
+				chatbotInput.value +
+				"</span>"
+			);
+			// console.log(chatbotInput.val())
+			chatbotInput.value = null;
+		}
 	});
 
 	// listen event from server
 	socket.on("response message", (data) => {
-		// console.log(data);
-		$("div#chat_fullscreen").append(
+		document.querySelector("div#chat_fullscreen").insertAdjacentHTML("beforeend",
 			"<span class='chat_msg_item chat_msg_item_admin'><div class='chat_avatar'><img src='../assets/chatbots-logo.png'/></div>" +
-			data +
-			"</span>"
+			data + "</span>"
 		);
 
 		// Auto scroll to last element
-		const chat_container = document.querySelector('#chat_fullscreen')
-		chat_container.scrollTop = chat_container.lastChild.offsetTop - 10
+		const chat_container = document.querySelector("#chat_fullscreen");
+		chat_container.scrollTop = chat_container.lastChild.offsetTop - 10;
 	});
 });
