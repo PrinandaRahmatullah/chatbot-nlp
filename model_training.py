eca2f1d42ca2d8589b5a6cdf8fe8984f73ecf607
PrinandaRahmatullah <prinanda50@gmail.com>
2024-09-21 21:57:51 +0700

    renew approach to match current tensorflow library and add callbacks

--- model_training.py
+++ model_training.py
@@ -4,14 +4,14 @@ import numpy as np
 import random
 import nltk
 import string
+# nltk.download('punkt')
 
 from matplotlib import pyplot as plt
 from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
 from Sastrawi.StopWordRemover.StopWordRemoverFactory import StopWordRemoverFactory
 from tensorflow.keras.models import Sequential, load_model
-from tensorflow.keras.layers import Dense, Activation, Dropout
-
-# from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 
 # Load file
 words = []
@@ -25,90 +25,90 @@ intents = json.loads(data_file)
 stemmer = StemmerFactory().create_stemmer()
 stopwords = StopWordRemoverFactory().get_stop_words()
 
-# Preporcess data
+# Preprocess data
 for intent in intents['intents']:
     for pattern in intent['input_patterns']:
-
         # Word tokenization
         pattern = nltk.word_tokenize(pattern)
         # Case folding
         pattern = [word.lower() for word in pattern]
-        # Filtering
+        # Filtering (remove stopwords, punctuation, and non-alphabetic tokens)
         pattern = [
             word for word in pattern if word not in stopwords and word.isalpha() and word not in string.punctuation]
         # Stemming
         pattern = [stemmer.stem(word) for word in pattern]
 
-        # insert to words list
+        # Add to words list
         words.extend(pattern)
 
-        # add doc in corpus
+        # Add document (pattern + tag)
         documents.append((pattern, intent['tag']))
 
-        # add tag to class list
+        # Add tag to class list if it's not already there
         if intent['tag'] not in classes:
             classes.append(intent['tag'])
-
 
 # Sort words and classes
 words = sorted(list(set(words)))
 classes = sorted(list(set(classes)))
 
-# Save to file in binary
+# Save words and classes to file (binary)
 pickle.dump(words, open('model/indo_words.pkl', 'wb'))
 pickle.dump(classes, open('model/indo_classes.pkl', 'wb'))
 
-
-# Create Data Training and Testing (In vector shape of words and classes)
+# Create training data (In vector form of words and classes)
 training = []
-# create an empty array for our output
-output_empty = [0] * len(classes)
+output_empty = [0] * len(classes)  # Create an empty output array
 
 # training set, bag of words for each sentence
 for doc in documents:
-    # initialize our bag of words
+    # Initialize our bag of words
     bag = []
-    # list of tokenized words for the pattern
     pattern_words = doc[0]
 
-    # create our bag of words array with 1, if word match found in current pattern
+    # Create bag of words array (1 if word is in pattern, 0 otherwise)
     for w in words:
         bag.append(1) if w in pattern_words else bag.append(0)
-        # output is a '0' for each tag and '1' for current tag (for each pattern)
-        output_row = list(output_empty)
-        output_row[classes.index(doc[1])] = 1
 
-    training.append([bag, output_row])  # feature and it class in binary
+    # Create output row (1 for the current tag, 0 for others)
+    output_row = list(output_empty)
+    output_row[classes.index(doc[1])] = 1
 
-# shuffle our features and turn into np.array
+    training.append([bag, output_row])
+
+# Shuffle and convert training data to numpy array
 random.shuffle(training)
-training = np.array(training)
-# create train and test lists. X - input_patterns, Y - intents
-X_train = list(training[:, 0])  # feature from words vector
-y_train = list(training[:, 1])  # label for class/tag
+training = np.array(training, dtype=object)
+
+# Split into X and y (features and labels)
+X_train = np.array(list(training[:, 0]))  # Feature from word vector
+y_train = np.array(list(training[:, 1]))  # Label for class/tag
+
 print("Training data created")
 
-
 # BUILD MODEL
-# Create model - 3 layers. First layer 128 neurons, second layer 64 neurons and 3rd output layer contains number of neurons
-# equal to number of intents to predict output intent with softmax
+# Create model: 3 layers (128, 64 neurons, output layer based on number of classes)
 model = Sequential()
 model.add(Dense(128, input_shape=(len(X_train[0]),), activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(64, activation='relu'))
 model.add(Dropout(0.5))
-model.add(Dense(len(y_train[0]), activation='softmax'))
+model.add(Dense(len(classes), activation='softmax'))  # Output layer for classes
+
 model.summary()
 
-# Report of model
-
-# Compile model.
-# sgd = SGD(lr=0.0015, decay=1e-6, momentum=0.9, nesterov=True)
+# Compile model
 model.compile(loss='categorical_crossentropy',
-              optimizer="adam", metrics=['accuracy'])
+              optimizer='adam', metrics=['accuracy'])
 
-# fitting and saving the model
-hist = model.fit(np.array(X_train), np.array(y_train),
-                 epochs=150, batch_size=5, verbose=1)
-model.save('model/model.h5', hist)
-print("model created")
+# Callbacks for EarlyStopping and ModelCheckpoint
+early_stop = EarlyStopping(monitor='loss', patience=10, restore_best_weights=True)
+model_checkpoint = ModelCheckpoint('model/best_model.h5', monitor='loss', save_best_only=True, verbose=1)
+
+# Fit model with callbacks
+hist = model.fit(X_train, y_train, epochs=150, batch_size=5, verbose=1, callbacks=[early_stop, model_checkpoint])
+
+# Save the final model (if not the best one)
+model.save('model/model.h5')
+
+print("Model created and saved")
